11e4870cc9360c7a5a363c6e8f3fb770d6d37fc9
PARK JI YOUNG <rwssss52@gmail.com>
2022-01-17 08:34:26 +0900

    박지영 ; 0114 숙제

---
+++ 0114H/0114H/main.cpp
@@ -0,0 +1,119 @@
+// HomeWork220114.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+//int Len(const char* _char)
+//{
+//	int count = 0;
+//	while (0 != _char[count]) count += 1;
+//	return count;
+//}
+
+void ToUpper(const char* _Left, char* _Right)
+{
+
+
+	for (int num = 0; _Left[num] != 0; num++) {
+
+
+		_Right[num] = _Left[num];
+
+		if ('a' <= _Left[num] && _Left[num] <= 'z') {
+			_Right[num] = _Left[num] - ('a' - 'A');
+		}
+
+	}
+
+}
+
+
+int ToInt(const char* _Left)
+{
+	int num = 0;
+	bool type = 0; //부호 플러그
+
+	//앞에 +가 붙어잇으면 무시한다.
+	if (*_Left == '+') _Left++;
+
+	//앞에 -가 있으면 type에 플러그 넣기
+	else if (*_Left == '-') {
+		type = true;
+		_Left++;
+	}
+
+	while (*_Left != '/0') {
+		//0~9이외의 문자열이라면 종료
+		if (*_Left < 48 || *_Left > 57) break;
+
+		num += *_Left - 48;
+		num *= 10; //자릿수를 올려준다
+		_Left++;
+	}
+
+	num /= 10; //루프에서 벗어나면 한자릿수 많기때문에 나눠주기
+
+	//만약 -부호가 붙어있다면
+	if (type) num = 0 - num;
+
+
+	return num;
+
+}
+
+// 좀 어려울거다.
+void CharChange(char* _Text, char _Prev, char _Next)
+{
+	for (int num = 0; _Text[num] != 0; num++) {
+
+
+		if (_Text[num] == _Prev) _Text[num] = _Next;
+
+	}
+
+
+}
+
+
+
+int main()
+{
+
+	{
+		char Text[5];
+
+		ToUpper("pP1231dcwsec", Text);
+	//	"12ABCCEEE"
+	}
+
+	{
+		char Text[100];
+
+		ToUpper("12abCcEee", Text);
+	//	"12ABCCEEE"
+
+			printf_s(Text);
+
+	}
+
+	{
+		char Text = '1';
+		int Count = '2';
+
+		//int Value = ToInt("15815");
+
+		printf("%d\n", ToInt("123"));
+	}
+
+	{
+		char Text[100] = "aa, bb, cc, dd";
+
+		CharChange(Text, ',', '|');
+		// aa| bb| cc| dd
+
+		printf_s(Text);
+	}
+
+
+}

c74a0a394951575cf34b88f0be89270040483fe7
PARK JI YOUNG <rwssss52@gmail.com>
2022-01-17 10:49:48 +0900

    박지영 : 0114 숙제

--- 0114H/0114H/main.cpp
+++ 0114H/0114H/main.cpp
@@ -3,31 +3,29 @@
 
 #include <iostream>
 
-
-//int Len(const char* _char)
-//{
-//	int count = 0;
-//	while (0 != _char[count]) count += 1;
-//	return count;
-//}
+//길이 구하는 함수
+int myLength(const char* _char)
+{
+	int count = 0;
+	while (0 != _char[count]) count += 1;
+	return count;
+}
 
 void ToUpper(const char* _Left, char* _Right)
 {
 
+	for (int i = 0; _Left[i] != 0; i++) {
 
-	for (int num = 0; _Left[num] != 0; num++) {
+		_Right[i] = _Left[i];
 
-
-		_Right[num] = _Left[num];
-
-		if ('a' <= _Left[num] && _Left[num] <= 'z') {
-			_Right[num] = _Left[num] - ('a' - 'A');
+		if ('a' <= _Left[i] && _Left[i] <= 'z') {
+			_Right[i] = _Left[i] - ('a' - 'A');
 		}
 
 	}
 
-}
 
+}
 
 int ToInt(const char* _Left)
 {
@@ -62,38 +60,64 @@ int ToInt(const char* _Left)
 
 }
 
-// 좀 어려울거다.
 void CharChange(char* _Text, char _Prev, char _Next)
 {
-	for (int num = 0; _Text[num] != 0; num++) {
+	for (int i = 0; _Text[i] != 0; i++) {
 
-
-		if (_Text[num] == _Prev) _Text[num] = _Next;
-
+		if (_Text[i] == _Prev) _Text[i] = _Next;
 	}
 
 
 }
 
+void Rervers(const char* _Sorce, char* _Result)
+{
+	int len = myLength(_Sorce)-1;
+
+	for (int i = 0; i <= len ; i++) {
+		char temp = _Sorce[i];			//임시 변수를 만들어서 제일 첫번째 것을 넣고
+		_Result[i] = _Sorce[len - i ];	//비어있는 곳에 마지막 요소를 넣는다
+		_Result[len - i ] = temp;		//마지막 요소에는 임시 변수에 들어가있는 것을 넣어 바꿔치기
+	}
+
+}
+
+void Cut(const char* _Sorce, int _Start, int _End, char* _Result)
+{
+
+	int resultNum = 0;
+
+	for(int i = 0; _Sorce[i] != 0; i++){
+	
+		if (_Sorce[i] >= _Start + 48 &&
+			_Sorce[i] <= _End + 48
+			) {
+
+			_Result[resultNum] = _Sorce[i];
+			resultNum++;
+		}
+	}
+
+}
 
 
 int main()
 {
 
-	{
-		char Text[5];
+	//{
+	//	char Text[5];
 
-		ToUpper("pP1231dcwsec", Text);
-	//	"12ABCCEEE"
-	}
+	//	ToUpper("pP1231dcwsec", Text);
+	////	"12ABCCEEE"
+	//}
 
 	{
-		char Text[100];
+		char Text[100] = " ";
 
 		ToUpper("12abCcEee", Text);
-	//	"12ABCCEEE"
+		//	"12ABCCEEE"
 
-			printf_s(Text);
+		printf_s("%s\n", Text);
 
 	}
 
@@ -112,8 +136,21 @@ int main()
 		CharChange(Text, ',', '|');
 		// aa| bb| cc| dd
 
-		printf_s(Text);
+		printf_s("%s\n", Text);
 	}
 
+	{
+		char Text[100] = { };
+		Rervers("0123456789", Text);
+		printf_s("%s\n",Text);
 
+	}
+
+	{
+		char Text[100] = { };
+		Cut("0123456789", 2, 7, Text);
+		// "234567"
+		printf_s(Text);
+
+	}
 }
